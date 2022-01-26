27de8ff7988e95823bca62936bc04550ff3a64e0
wabwhc <jack-daniel@naver.com>
2022-01-26 14:19:32 +0900

    finish

--- orbit.js
+++ orbit.js
@@ -7,7 +7,8 @@ export default class orbit{
         this.ctx = ctx;
         this.oribt = 100;
         this.imfor = [];
-        for(let i = 0; i < 7; i++){
+        this.ballcount = parseInt(prompt('개수'))
+        for(let i = 0; i < this.ballcount; i++){
             this.imfor[i] = [];
         }
         this.balls = []
@@ -16,7 +17,7 @@ export default class orbit{
     }
 
     makeball(){
-        for(let i = 0; i < 7; i++){
+        for(let i = 0; i < this.ballcount; i++){
             this.fillStyle = "#" + Math.round(Math.random() * 0xffffff).toString(16);
             this.balls[i] = new ball(this.x, this.y, this.ctx, (this.oribt += 20), this.fillStyle);
             this.imfor[i][0] = this.fillStyle;
@@ -26,7 +27,7 @@ export default class orbit{
     }
 
     orbitLine(){
-        for(let i = 0; i < 7; i++){
+        for(let i = 0; i < this.ballcount; i++){
             this.ctx.beginPath();
             this.ctx.strokeStyle = this.imfor[i][0];
             this.ctx.arc(this.x / 2, this.y / 2, this.imfor[i][1], 0, Math.PI * 2, false);
@@ -38,7 +39,7 @@ export default class orbit{
 
     drawBall(){
         this.ctx.clearRect(0,0,this.x, this.y);
-        for(let i =0; i < 7; i++){
+        for(let i =0; i < this.ballcount; i++){
             this.balls[i].draw();
         }
         requestAnimationFrame(this.drawBall.bind(this))
